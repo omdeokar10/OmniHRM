49ab7d1c9de759b9d9a5b69c3d476434807fc0fc
kaustubhdeokar <kaustubhd9@gmail.com>
2024-07-16 23:33:54 +0530

    auth added frontend

---
+++ hrm-ui/src/components/auth/LoginComponent.jsx
@@ -0,0 +1,98 @@
+import React from 'react'
+import { useState } from 'react';
+import { loginAPICall, saveLoggedInUser, storeToken } from '../../service/auth/AuthService';
+import { useNavigate } from 'react-router-dom';
+
+function LoginComponent() {
+
+    const [userName, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigator = useNavigate();
+
+    async function handleLoginForm(e) {
+
+        e.preventDefault();
+
+        const loginDto = { userName, password };
+        storeToken(null);
+
+        await loginAPICall(loginDto).then((response) => {
+            console.log(response.data);
+
+            const token = 'Bearer ' + response.data.jwtToken;
+            const username = response.data.username;
+            const roles = response.data.roles;
+            storeToken(token);
+
+            saveLoggedInUser(username, roles);
+            navigator("/performance/summary")
+
+            window.location.reload(false);
+        }).catch(error => {
+            console.error(error);
+        })
+
+    }
+
+    return (
+        <div className='container'>
+            <br /> <br />
+            <div className='row'>
+                <div className='col-md-6 offset-md-3'>
+                    <div className='card'>
+                        <div className='card-header'>
+                            <h2 className='text-center'> Login </h2>
+                        </div>
+
+                        <div className='card-body'>
+                            <form>
+
+                                <div className='row mb-3'>
+                                    <label className='col-md-3 control-label'> Username</label>
+                                    <div className='col-md-9'>
+                                        <input
+                                            type='text'
+                                            name='username'
+                                            className='form-control'
+                                            placeholder='Enter username'
+                                            value={userName}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                        >
+                                        </input>
+                                    </div>
+                                </div>
+
+                                <div className='row mb-3'>
+                                    <label className='col-md-3 control-label'> Password </label>
+                                    <div className='col-md-9'>
+                                        <input
+                                            type='password'
+                                            name='password'
+                                            className='form-control'
+                                            placeholder='Enter password'
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        >
+                                        </input>
+                                    </div>
+                                </div>
+
+                                <div className='form-group mb-3'>
+                                    <button className='btn btn-primary' onClick={(e) => handleLoginForm(e)}>Submit</button>
+
+                                </div>
+                            </form>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
+}
+
+export default LoginComponent
---
+++ hrm-ui/src/service/auth/AuthService.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import React from "react";
+
+const baseURL = "http://localhost:8081/api/auth";
+//localhost:8081/api/auth/login
+export const registerAPICall = (registerObj) => axios.post(baseURL + '/register', registerObj);
+
+export const loginAPICall = (loginObj) => {
+    return axios.post(baseURL + '/login', loginObj);
+}
+
+export const storeToken = (token) => localStorage.setItem("token", token);
+
+export const getToken = () => localStorage.getItem("token");
+
+export const saveLoggedInUser = (username, roles) => {
+    sessionStorage.setItem("authenticatedUser", username);
+    sessionStorage.setItem("roles", roles);
+}
+
+export const isUserLoggedIn = () => {
+
+    const username = getLoggedInUser();
+
+    if (username == null) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+export const getLoggedInUser = () => {
+    return sessionStorage.getItem("authenticatedUser");
+}
+
+export const logout = () => {
+    localStorage.clear();
+    sessionStorage.clear();
+}
+
+export const isAdminUser = () => {
+
+    let role = sessionStorage.getItem("role");
+
+    if (role != null && role === 'ROLE_ADMIN') {
+        return true;
+    } else {
+        return false;
+    }
+
+}
